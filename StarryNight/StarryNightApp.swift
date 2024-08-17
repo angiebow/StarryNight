caba2b70e10c7e4c43c19f8370091f9269811528
Pelangi Masita <pelangimasita18@gmail.com>
2024-08-17 16:51:29 +0700

    Initial Commit

---
+++ StarryNight/StarryNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarryNightApp.swift
+//  StarryNight
+//
+//  Created by Pelangi Masita Wati on 17/08/24.
+//
+
+import SwiftUI
+
+@main
+struct StarryNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
